93b2c3e4b38d5479f3d6cfbe35bacc17423e16aa
danielalanbates <danielalanbates@gmail.com>
2025-10-27 16:14:23 -0700

    Update for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'static',
-  site: 'https://yourusername.github.io',
-  // base: '/repository-name', // Uncomment and update if not using custom domain
+  site: 'https://batesai.org',
 });
